54df82b6d1277e54d9644d8392d988f6768b29f6
mferferreira <a2018114425@estudiantes.upsa.edu.bo>
2020-08-30 23:29:26 -0400

    con controlador de student mas autorizacion

---
+++ ApiSeccureStudent/ApiSeccureStudent/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ApiSeccureStudent.Models
+
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext 
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ApiSeccureStudent.Models.Student> Students { get; set; }
+    }
+}
