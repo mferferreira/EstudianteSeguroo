4bb48051330098c6eb0235d72ff79f52160a8627
mferferreira <a2018114425@estudiantes.upsa.edu.bo>
2020-09-06 23:26:10 -0400

    se agrego el proyecto de administracion mvc

---
+++ ApiSeccureStudent/admStudentSecurityy/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace admStudentSecurityy.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<admStudentSecurityy.Models.Student> Students { get; set; }
+    }
+}
